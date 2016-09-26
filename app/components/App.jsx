df2937e979223599fc9e30062d70f77c02cab3c8
p-tvo-hannd <p-tvo-hannd>
2016-09-26 17:07:22 +0700

    2016/09/26

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,7 +1,60 @@
 import React from 'react';
-import Note from './Note.jsx';
+import uuid from 'node-uuid';
+import Notes from './Notes';
+
 export default class App extends React.Component {
-    render() {
-        return (<Note/>);
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn Webpack'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        }
+    }
+
+    addNote = ()=> {
+        this.setState({
+            notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: 'New task'
+            }])
+        });
+    };
+
+    editNote = (id, task) =>{
+        if (!task.trim()) {
+            return;
+        }
+        var notes = this.state.notes.map(
+            note=> {
+                if (note.id === id && task) {
+                    note.task = task
+                }
+                return note;
+            });
+        this.setState({notes});
+    }
+
+    render(){
+        let notes = this.state.notes;
+        return (
+            <div>
+                <button onClick={this.addNote}>+</button>
+                <Notes notes={notes}
+                       onEdit={this.editNote}
+                />
+            </div>
+        )
     }
 }
